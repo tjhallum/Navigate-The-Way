38e62228cc8a6226f993e6a8fa2ac572d67f9658
Ted Hallum <53157308+tjhallum@users.noreply.github.com>
2026-03-16 02:11:22 -0400

    Add lastmod dates to generated sitemap

--- scripts/generate-sitemap.sh
+++ scripts/generate-sitemap.sh
@@ -4,12 +4,18 @@ set -euo pipefail
 ROOT_URL="https://www.navtheway.com"
 DOCS_DIR="docs"
 OUTPUT_FILE="${DOCS_DIR}/sitemap.xml"
+HOME_PAGE="${DOCS_DIR}/index.html"
+
+lastmod_date() {
+  date -u -r "$1" '+%Y-%m-%d'
+}
 
 {
   echo '<?xml version="1.0" encoding="UTF-8"?>'
   echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
   echo '  <url>'
   echo "    <loc>${ROOT_URL}/</loc>"
+  echo "    <lastmod>$(lastmod_date "${HOME_PAGE}")</lastmod>"
   echo '  </url>'
 
   while IFS= read -r page; do
@@ -18,8 +24,9 @@ OUTPUT_FILE="${DOCS_DIR}/sitemap.xml"
 
     echo '  <url>'
     echo "    <loc>${ROOT_URL}/${page_name}</loc>"
+    echo "    <lastmod>$(lastmod_date "${page}")</lastmod>"
     echo '  </url>'
-  done < <(find "${DOCS_DIR}" -maxdepth 1 -type f -name '*.html' | sort)
+  done < <(find "${DOCS_DIR}" -maxdepth 1 -type f -name '*.html' | LC_ALL=C sort)
 
   echo '</urlset>'
 } > "${OUTPUT_FILE}"
